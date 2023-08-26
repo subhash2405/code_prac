00e8c9c85c2dc8554f1bb28a2702069d821b5e36
subhash2405 <subhashchandrap456@gmail.com>
2023-08-26 23:03:14 +0530

    Wrote a code to check anagrams

--- anagram.py
+++ anagram.py
@@ -1,1 +1,15 @@
-
+str1=input("enter first word:")
+str2=input("enter second word:")
+a=0
+if(len(str1)!=len(str2)):
+    print('False')
+else:
+    str1=''.join(sorted(str1))
+    str2=''.join(sorted(str2))
+    for i in range(len(str1)):
+        if(str1[i]==str2[i]):
+            a+=1
+    if(a==len(str1)):
+        print('True')
+    else:
+        print('False')
